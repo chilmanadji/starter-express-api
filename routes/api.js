b9bbca88135162686093bc1bc059c6506c92342e
chilmanaji <chilman.adji@gmail.com>
2023-04-10 00:24:17 +0700

    print pretty json

--- routes/api.js
+++ routes/api.js
@@ -17,7 +17,7 @@ router.get('/ethica', function(req, res, next) {
 
 router.get('/ethica/loadproduct/:key', function(req, res, next) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+   
    
   var url = 'https://api.ethica.id/public/master_barang/loaddata_eksternal?customer_seq=0979'+req.params.key;
   const https = require('https');
@@ -31,7 +31,9 @@ router.get('/ethica/loadproduct/:key', function(req, res, next) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => { 
-      res.send(data);
+      var result = JSON.parse(data);
+      res.header("Content-Type",'application/json');
+      res.send(JSON.stringify(result, null, 3));
     });
 
   }).on("error", (err) => {
@@ -41,7 +43,7 @@ router.get('/ethica/loadproduct/:key', function(req, res, next) {
 
 router.get('/ethica/search/:search', function(req, res, next) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  
  
   var url = 'https://api.ethica.id/public/master_barang/loaddata_eksternal?customer_seq=0979&offset=0&search='+req.params.search;
   const https = require('https');
@@ -55,7 +57,9 @@ router.get('/ethica/search/:search', function(req, res, next) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => { 
-      res.send(data);
+      var result = JSON.parse(data);
+      res.header("Content-Type",'application/json');
+      res.send(JSON.stringify(result, null, 3));
     });
 
   }).on("error", (err) => {
@@ -66,7 +70,7 @@ router.get('/ethica/search/:search', function(req, res, next) {
 
 router.get('/ethica/latest', function(req, res, next) {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+ // res.setHeader('Content-Type', 'text/plain');
 
   var url = 'https://api.ethica.id/public/master_barang/loaddata_eksternal?customer_seq=0979&offset=0&is_ada_stok=T&order_by=tahun+DESC';
   const https = require('https');
@@ -80,7 +84,10 @@ router.get('/ethica/latest', function(req, res, next) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => { 
-      res.send(data);
+      var result = JSON.parse(data);
+      res.header("Content-Type",'application/json');
+      res.send(JSON.stringify(result, null, 3));
+      console.log(JSON.parse(data));
     });
 
   }).on("error", (err) => {
