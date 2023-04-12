5289a73ece2b6b4bf090a2890e70e1f0b28757a2
chilmanaji <chilman.adji@gmail.com>
2023-04-13 03:26:55 +0700

    update

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,7 @@ var router = express.Router();
  
 
 router.get('*',function(req, res, next) {
-  /*var url = req.originalUrl;
+  var url = req.originalUrl;
   url = url.replace('/http:','http:');
   const https = require('http');
   https.get(url, (resp) => {
@@ -16,7 +16,7 @@ router.get('*',function(req, res, next) {
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => { 
-      data = data.replace('http://ethica.id')
+      
       var result = data;
       res.header("Content-Type",'text/html');
       res.send(data);
@@ -25,7 +25,7 @@ router.get('*',function(req, res, next) {
   }).on("error", (err) => {
     console.log("Error: " + err.message);
   }); 
-  */
-  res.send(req.rawHeaders[1]);
+  
+  //res.send(req.rawHeaders[1]);
 });
 module.exports = router;
